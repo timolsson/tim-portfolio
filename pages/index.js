1c8c8fc8b5c361c164662f5836fbe490d2f75b50
Tim Olsson <55507423+timolsson@users.noreply.github.com>
2021-05-14 22:44:05 +0200

    Globals + Content
    - Globals + fonts added
    - Text content added

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <Grid container direction="row" className="testing">
-        <Grid container item xs="11" justify="flex-start" alignItems="center">
-          <Box>
+      <Grid container direction="row" className="home-container">
+        <Grid container item xs="11" justify="flex-start" alignContent="center">
+          <Box className="textbox">
             <ul>
               <li>
                 <Link href="/showcase">Showcase</Link>
@@ -18,9 +18,18 @@ export default function Home() {
                 <Link href="/showcase">Contact</Link>
               </li>
             </ul>
+            <span className="name">
+              <h1>Hello,</h1>
+              <h1>my name is Tim Olsson</h1>
+            </span>
+          </Box>
+
+          <Box className="role">
+            <h1>Front-end Developer</h1>
           </Box>
         </Grid>
-        <Grid item xs="1">
+        <Grid item container xs="1" className="socials" direction="column">
+          <Link href="mailto:hello@timolsson.com">Email</Link>
           <Link href="LinkedIn">LinkedIn</Link>
         </Grid>
       </Grid>
